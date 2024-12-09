71e740b83efe10627310607f8f8e6ac795d1d4b7
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-09 10:14:08 +0100

    Updated layout.js with Footer component

--- app/app/layout.js
+++ app/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
       <body className="main">
         <ResponsiveNavbar />
         {children}
+        <Footer />
       </body>
       
     </html>
